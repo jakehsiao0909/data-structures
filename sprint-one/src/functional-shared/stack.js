c5936853cf398a291abc4473b3f37ae59124be4a
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-07 15:34:33 -0600

    starting part of sprint two

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  extend(output, stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+var extend = function(obj, methods ){
+  for(var key in methods) {
+    obj[key] = methods[key];
+  }
+
+};
+
+var stackMethods = {
+
+  push: function(value) {
+  },
+  pop: function() {
+  },
+
+  size: function() {
+  }
+
+};
 
 
