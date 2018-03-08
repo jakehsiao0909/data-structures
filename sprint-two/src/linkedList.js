20c7b51eaba0300168e4eb4c0d38189551c9122c
Jake Hsiao <jakehsiao@Jakes-MacBook-Pro.local>
2018-03-08 11:12:48 -0600

    linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    list.head = list.head || node;
+
+    if(list.tail) {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function() {
+    var removed = list.head;
+    list.head = list.head.next;
+    return removed.value
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
