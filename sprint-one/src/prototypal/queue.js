64055f10bb093ddca43efa4f837e4311ce73ef07
Jake Hsiao <jakehsiao@Jakes-MacBook-Pro.local>
2018-03-07 19:18:58 -0600

    completed pseudoclassical

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,41 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  enqueue: function(value) {
+    if (this.count > 0) {
+      var tempStore = {};
+      for (var key in this.storage) {
+        tempStore[parseInt(key) + 1] = this.storage[key];
+      }
+      tempStore[0] = value;
+      this.storage = tempStore;
+    }
+    else {
+      this.storage[this.count] = value;
+    }
+    this.count++;
+  },
+
+  dequeue: function() {
+    this.count--;
+    if (this.count < 0) this.count = 0;
+    var removed = this.storage[this.count];
+    delete this.storage[this.count];
+    return removed;
+  },
+
+  size: function(){
+    return this.count;
+  }
+};
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
 
+Queue.prototype.enqueue = function(value) {
+  if (this.count > 0) {
+    var tempStore = {};
+    for (var key in this.storage) {
+      tempStore[parseInt(key) + 1] = this.storage[key];
+    }
+    tempStore[0] = value;
+    this.storage = tempStore
+  }
+  else {
+    this.storage[this.count] = value;
+  }
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  this.count--;
+  if (this.count < 0) this.count = 0;
+  var removed = this.storage[this.count];
+  delete this.storage[this.count];
+  return removed;
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+}
