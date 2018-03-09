ca714298cb07a3f1dd068c57fd64c18ef5b47c68
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-09 10:21:02 -0600

    done with BinaryTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,66 @@
 var BinarySearchTree = function(value) {
+  var someTree = Object.create(BinarySearchTree.prototype)
+
+  someTree.left = null;
+  someTree.right = null;
+  someTree.value = value;
+  return someTree;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+
+
+  if (this.value > value) {
+    if (!this.left) {
+      this.left = BinarySearchTree(value);
+    }
+    else {
+      this.left.insert(value)
+    }
+  }
+
+  if (this.value < value) {
+    if (!this.right) {
+      this.right = BinarySearchTree(value);
+    }
+    else {
+      this.right.insert(value)
+    }
+  }
+};
+
+
+BinarySearchTree.prototype.contains = function(target) {
+  if (this.value === target) return true;
+
+  if (this.value > target) {
+    if (this.left) {
+      return this.left.contains(target)
+    }
+  }
+
+  if (this.value < target) {
+    if (this.right) {
+      return this.right.contains(target)
+    }
+  }
+
+  return false;
+};
+
+
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  function recurseTree(tree){
+    callback.call(tree,tree.value);
+    if(tree.left !== null){
+      recurseTree(tree.left);
+    }
+    if(tree.right !== null){
+      recurseTree(tree.right);
+    }
+  }
+  recurseTree(this);
 };
 
 
