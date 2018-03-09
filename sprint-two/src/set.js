bfba47276eabee9e6023e04473e121d5054f4210
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-08 19:19:31 -0600

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
+  set.size = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
+  this.size++;
 };
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  var removed = this._storage[item]
+  delete this._storage[item]
+  return removed;
 };
 
 /*
