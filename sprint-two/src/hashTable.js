2fc3ea4ef9e28c9f5299526cea2eccda6adc3af1
jakehsiao0909 <jakehsiao0909@gmail.com>
2018-03-08 20:02:43 -0600

    hashtable prototype

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,61 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var check = false;
+  var bucket = this._storage.get(index);
+
+  if (!bucket) {
+    bucket = [];
+    this._storage.set(index, bucket);
+  }
+
+  bucket.forEach(function(tuple) {
+    if (tuple[0] === k) {
+      tuple[1] = v;
+      check = true;
+    }
+  });
+  
+  
+    if (!check) {
+      bucket.push([k, v]);
+      this.count++;
+    }
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  if (!this._storage.get(index)) return null;
+  var bucket = this._storage.get(index);
+
+  var lookup;
+
+  bucket.forEach(function(tuple) {
+    if (tuple[0] === k) lookup = tuple[1];
+  });
+  return lookup;
+  
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(index)) return null;
+
+  var bucket = this._storage.get(index);
+
+  bucket.forEach(function(tuple, index) {
+    if (tuple[0] === k) {
+      bucket.splice(index, 1);
+    }
+    this.count--;
+  });
 };
 
 
